149de7e625ca412e61d8116076e3ba63dd73f4d1
KabdualiBek <34888928+KabdualiBek@users.noreply.github.com>
2020-04-02 19:39:55 +0600

    Converges, but less than one

--- GrassbergerProcaсcia.c
+++ GrassbergerProcaсcia.c
@@ -2,18 +2,18 @@
 #include <math.h>
 
 int main(){
-    int N = 5000, m, n, k, i;
-    double a = 2, b = 1, t = 0, tau = 0.01, C_k_l, l, rho, s[2][N];
+    int N = 15000, m, n, k, i;
+    double a = 2, b = 1, t = 0, tau = 0.005, C_k_l, l, rho, s[2][N];
     for (int i = 0; i < N; i++){
-        s[0][i] = a*sin(t);
-        s[1][i] = b*cos(t);
+        s[0][i] = a*cos(t);
+        s[1][i] = b*sin(t);
         t += tau;
     }
     scanf("%d", &k);
     printf("k = %d.   ", k);
     // for (k = 3; k < 10; k++){
-        l = 0.25;
-        while (l > 0.000001){
+        l = 0.0625;
+        for (int j = 0; j < 10; j++){
             C_k_l = 0;
             for (m = 0; m < N - k; m++){
                 for (n = 0; n < N - k; n++){
@@ -22,9 +22,9 @@ int main(){
                         a = s[0][n + i] - s[0][m + i];
                         b = s[1][n + i] - s[1][m + i];
                         rho = a*a + b*b;
-                        if (rho < l)
-                            C_k_l += 1;
                     }
+                    if (rho < l*l)
+                        C_k_l += 1;
                 }
             }
             C_k_l /= N*N;
