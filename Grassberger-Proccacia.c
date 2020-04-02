f8790dab435d5a0dfb560149707285c5a2ee456c
KabdualiBek <34888928+KabdualiBek@users.noreply.github.com>
2020-04-02 16:52:35 +0600

    1 push

---
+++ Grassberger-Proccacia.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int N = 10000, m, n, k, i;
+    double a = 2, b = 1, t = 0, tau = 0.01, C_k_l, l, rho, s[2][N];
+    for (int i = 0; i < N; i++){
+        s[0][i] = a*sin(t);
+        s[1][i] = b*sin(t);
+        t += tau;
+    }
+    k = 4;
+    // for (k = 3; k < 10; k++){
+        l = 0.25;
+        while (l > 0.000001){
+            C_k_l = 0;
+            for (m = k; m < N; m++){
+                for (n = k; n < N; n++){
+                    rho = 0;
+                    for (i = 0; i < k; i++){
+                        a = s[0][n - k + i] - s[0][m - k + i];
+                        b = s[1][n - k + i] - s[1][m - k + i];
+                        rho = a*a + b*b;
+                        if (rho < l)
+                            C_k_l += 1;
+                    }
+                }
+            }
+            C_k_l /= N*N;
+            printf("%lf, ", log(C_k_l)/log(l));
+            l /= 2;
+        }
+        printf("\n");
+    // }
+    return 0;
+}
